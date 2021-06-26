b3cc78cee59e4503ca439e2caf758e2eb0f56f32
Avinash Tomar <avi.tomar1405@gmail.com>
2021-06-25 21:06:55 -0400

    Foundation Selection Sort

---
+++ src/com/kickinterview/sorting/foundation/SelectionSort.java
@@ -0,0 +1,40 @@
+package com.kickinterview.sorting.foundation;
+
+public class SelectionSort {
+
+    /**
+     * function selectionsort(A): //array A[1..n]
+     * for i in 1 to n:
+     * #Find the i th smallest element and swap it
+     * with A[i]
+     * min = i
+     * During a linear scan of A[i..n]:
+     * if you find a smaller element than
+     * A[min]:
+     * Update the index of the min
+     * element
+     * Swap the min element with A[i]
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,3,1,5,8};
+        selectionSort(nums);
+        for(int i:nums)
+            System.out.println(i);
+    }
+
+    private static void selectionSort(int[] nums) {
+        int min;
+        int len = nums.length;
+        for(int i=0;i<len;i++){
+            min = i;
+            for(int j=i+1;j<len;j++)
+                if(nums[j]<nums[min])
+                    min = j;
+
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min] = temp;
+        }
+    }
+
+}
